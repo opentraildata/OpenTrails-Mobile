04aa02d2ad00b50f9d9cab4b840f6fa9db7c7b0d
Anselm Bradford <git@anselmbradford.com>
2014-04-24 21:31:30 -0700

    Adds grunt watch for html and js
    Adds ability for grunt to watch html and js in addition to css.

--- gruntfile.js
+++ gruntfile.js
@@ -1,22 +1,48 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-        less: {
-            development: {
-                options: {
-                    paths: ["./www/less"],
-                    yuicompress: true
-                },
-                files: {
-                    "./www/css/application.css": "./www/less/application.less",
-                    "./platforms/ios/www/css/application.css": "./www/less/application.less"
-                }
-            }
-        },
-        watch: {
-            files: "./www/less/*",
-            tasks: ["less"]
-        }
-    });
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.initConfig({
+  less: {
+    development: {
+      options: {
+        paths: ["./www/less"],
+        yuicompress: true
+      },
+      files: {
+        "./www/css/application.css": "./www/less/application.less"
+      }
+    } // development
+  }, // less
+  watch: {
+    scripts: {
+      files: ['./www/js/**/*.js'],
+      tasks: ['copy:js']
+    }, //scripts
+    less: {
+      files: ['./www/less/*'],
+      tasks: ['less','copy:css']
+    }, //less
+    html: {
+      files: ['./www/index.html'],
+      tasks: ['copy:html']
+    } //html
+  }, // watch
+  copy: {
+    css: {
+      src: './www/css/application.css',
+      dest: './platforms/ios/www/css/application.css',
+      filter: 'isFile'
+    }, //css
+    js: {
+      src: './www/js/',
+      dest: './platforms/ios/www/js/'
+    }, //js
+    html: {
+      src: './www/index.html',
+      dest: './platforms/ios/www/index.html',
+      filter: 'isFile'
+    } //html
+  } //copy
+  });
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 };
